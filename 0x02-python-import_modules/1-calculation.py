0e765583dc5d4fe23167c8b22af794cd45ca079c
techmate-dot <snwankwo218@gmail.com>
2022-06-02 18:34:25 -0700

    task 1 cal

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
+
+print('10 + 5 = {}'.format(add(10, 5)))
+print('10 - 5 = {}'.format(sub(10, 5)))
+print('10 * 5 = {}'.format(mul(10, 5)))
+print('10 / 5 = {}'.format(div(10, 5)))
