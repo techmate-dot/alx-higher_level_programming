8d23988f270e2e4c1a0197c6c76db5af0041aa55
Samuel Nwankwo Chisom <88623647+techmate-dot@users.noreply.github.com>
2022-06-03 09:28:58 +0100

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
     print('{:d} + {:d} = {:d}'.format(a, b, add(10, 5)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(10, 5)))
     print('{:d} * {:d} = {:d}'.format(a, b, mul(10, 5)))
