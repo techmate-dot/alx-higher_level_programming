d159b9c613a90323b789cf751000e6599aeb57f6
techmate-dot <snwankwo218@gmail.com>
2022-06-02 18:47:59 -0700

    task 1 cal

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,10 @@
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
 
+a = 10
+b = 5
 
-print('10 + 5 = {}'.format(add(10, 5)))
-print('10 - 5 = {}'.format(sub(10, 5)))
-print('10 * 5 = {}'.format(mul(10, 5)))
-print('10 / 5 = {}'.format(div(10, 5)))
+print('{} + {} = {}'.format(a, b, add(10, 5)))
+print('{} - {} = {}'.format(a, b, sub(10, 5)))
+print('{} * {} = {}'.format(a, b, mul(10, 5)))
+print('{} / {} = {}'.format(a, b, div(10, 5)))
