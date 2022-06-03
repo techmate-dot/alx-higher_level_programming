35f453b471be224b70c85f58dece0feaf4490317
techmate-dot <snwankwo218@gmail.com>
2022-06-03 02:22:55 -0700

    task 2 print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import sys
-lists = sys.argv
-length = len(sys.argv)
-print(f"{length - 1} arguments:")
-for i in range(1, length):
-    print(f"{i}: {lists[i]}")
+if __name__ == '__main__':
+    import sys
+    lists = sys.argv
+    length = len(sys.argv)
+    if length == 1:
+        print(f"{length - 1} arguments.")
+    else:
+        print(f"{length - 1} arguments:")
+    for i in range(1, length):
+        print(f"{i}: {lists[i]}")
