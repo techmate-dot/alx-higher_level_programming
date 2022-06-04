b2ea3651bb286ccaae610477e7c8616e6287f8aa
techmate-dot <snwankwo218@gmail.com>
2022-06-03 18:38:40 -0700

    test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,17 @@ if __name__ == '__main__':
     solve = {'+': add, '-': sub, "*": mul, '/': div}
 
     length = len(argv)
-    if (length - 1) == 3:
+    if (length) == 4:
         operator = argv[2]
 
-    if (length - 1) != 3:
+    if (length) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(1)
+        exit(1)
     elif operator not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and ")
-        print(1)
+        exit(1)
     else:
         a = int(argv[1])
         b = int(argv[3])
         result = solve[operator](a, b)
-        print(f"{a} {operator} {b} = {result}")
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
