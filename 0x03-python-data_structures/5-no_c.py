ba9144a5abf1c0f3bd84cca5b08ccb31063f8d0c
techmate-dot <snwankwo218@gmail.com>
2022-06-07 06:21:33 -0700

    task 5 && task 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,9 @@
 
 def no_c(my_string):
     lc = len(my_string)
-    for i in range()
+    new_string = ''
+    for i in range(lc):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+
+    return new_string
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
-my_list = [1, 3, 4, 'H', 7]
-print_reversed_list_integer(my_list)
+matrix = [[]]
+
+print_matrix_integer(matrix)
