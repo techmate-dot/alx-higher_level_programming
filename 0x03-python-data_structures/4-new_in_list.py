d295ae50c55dede996d2751a146a76fabe6b56a5
techmate-dot <snwankwo218@gmail.com>
2022-06-07 04:01:28 -0700

    task & task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    l = len(my_list)
+    lenght = len(my_list)
     if idx < 0:
         return my_list
-    elif idx >= l:
+    elif idx >= lenght:
         return my_list
     else:
         list_copy = my_list.copy()
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    lc = len(my_string)
+    for i in range()
