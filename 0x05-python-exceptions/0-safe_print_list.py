96ef0c9437245deab41d5a837de6eb078d9bbc3d
techmate-dot <snwankwo218@gmail.com>
2022-06-20 02:52:33 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        num = 0
+        for i in range(x):
+            print(my_list[i], end='')
+            num += 1
+    finally:
+        print('')
+        return num
