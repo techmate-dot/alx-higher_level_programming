14e6ae21e3f1f3a57211be19314d4d2484e43d91
techmate-dot <snwankwo218@gmail.com>
2022-06-20 08:31:50 -0700

    task 3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 num += 1
         print('')
         return num
-    except IndexError:
+    except ValueError:
         print('')
         return num
--- 0x05-python-exceptions/t3.py
+++ 0x05-python-exceptions/t3.py
@@ -4,7 +4,7 @@ safe_print_list_integers = \
 
 my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list_integers(my_list, 6)
+nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
