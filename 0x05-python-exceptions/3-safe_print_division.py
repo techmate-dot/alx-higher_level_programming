b96842be443c53dbe30f18bbf3de044002f5a151
techmate-dot <snwankwo218@gmail.com>
2022-06-20 10:13:59 -0700

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,9 @@
 def safe_print_division(a, b):
     try:
         ret = a / b
-        return ret
     except ZeroDivisionError:
         ret = None
-        return None
     finally:
         print("Inside result: ", end='')
         print("{}".format(ret))
+    return ret
