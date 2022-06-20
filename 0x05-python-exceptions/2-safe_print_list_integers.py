68371509aff7ee1c1f9b859e9191052b1fe7d101
techmate-dot <snwankwo218@gmail.com>
2022-06-20 08:27:51 -0700

    task 3

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end='')
+                num += 1
+        print('')
+        return num
+    except IndexError:
+        print('')
+        return num
