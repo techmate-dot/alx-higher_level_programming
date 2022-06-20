a766b49a1b9808b1549457b7b7ff4ed77ad6b411
techmate-dot <snwankwo218@gmail.com>
2022-06-20 08:36:48 -0700

    task 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             num += 1
         print('')
         return num
-    except IndexError:
+    except ValueError:
         print('')
         return num
