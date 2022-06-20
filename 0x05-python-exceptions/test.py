ba599e177a212ea01e4b76fff2ef4792e21d783c
techmate-dot <snwankwo218@gmail.com>
2022-06-20 11:30:47 -0700

    Task 4

--- 0x05-python-exceptions/test.py
+++ 0x05-python-exceptions/test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+list_division = __import__('4-list_division').list_division
 
-my_list = [1, 2, 3, 4, 5]
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
