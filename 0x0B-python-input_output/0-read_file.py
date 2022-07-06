0c0c4a86e07819c01d436134b0ea7c3f032664d9
techmate-dot <snwankwo218@gmail.com>
2022-07-06 17:26:07 +0100

    Task 0 && Task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+
+""" print content of a file to the standard output"""
+
+
 def read_file(filename=""):
+    """
+        args:
+            filename - Name of the text file
+        Returns: prints the content of the file to stdout
+    """
     with open(filename, 'r', encoding='UTF8') as f:
         value = f.read()
     print(value)
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,11 @@
+
 def write_file(filename="", text=""):
+    """
+        Args:
+            filename - name of the file you want to write to
+            ext - The the text you want to write
+    """
+
     with open(filename, 'w', encoding='UTF8') as f:
         num_char = f.write(text)
     return num_char
