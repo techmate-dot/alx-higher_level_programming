85f7babfb651c29eedcf1fdface27faee7e82cfd
techmate-dot <snwankwo218@gmail.com>
2022-07-07 01:24:30 +0100

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,20 +6,6 @@
 import json
 
 
-def to_json_string(my_obj):
-    """  A function that returns the JSON representation of an object (string):
-
-    Args:
-        my_obj (str): The string to be converted
-
-    Returns:
-        str: returns the JSON representation of an object (string):
-    """
-
-    formated_str = json.dumps(my_obj)
-    return formated_str
-
-
 def save_to_json_file(my_obj, filename):
 
     """ Writes json to a file
@@ -28,8 +14,6 @@ def save_to_json_file(my_obj, filename):
         int: The number of char added
     """
 
-    formated_str = to_json_string(my_obj)
-
     with open(filename, 'w', encoding='UTF-8') as file:
-        num_char = file.write(formated_str)
+        num_char = json.dump(my_obj, file)
     return num_char
