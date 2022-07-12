aee269b75277af997afffa4356aa1a916e723f00
techmate-dot <snwankwo218@gmail.com>
2022-07-12 15:43:37 +0100

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by: (based on 3-square.py)
+    """
+
+
+class Square:
+    """Attributes:
+            size: privates class that hold the square size
+
+        """
+    def __init__(self, size=0):
+        """_summary_
+
+        Args:
+            size (int, optional): size. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    @property
+    def size(self):
+        """ A method that returns the size of square
+
+        Returns:
+            int: size of square
+        """
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        self._Square__size = value
+
+    def area(self):
+        """Returns the area of square
+
+        Returns:
+            int: area
+        """
+        return self._Square__size * self._Square__size
