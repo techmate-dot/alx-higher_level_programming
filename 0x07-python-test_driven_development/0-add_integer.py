0c0382d3ea0078e2fcd886191580fd1735277c40
techmate-dot <snwankwo218@gmail.com>
2022-07-16 11:24:48 +0100

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Returns the addition of two arguments
+>>>add_integer(2, 3)
+5
+    """
+
+
+def add_integer(a, b=98):
+    """A function that adds two integers
+    Returns (int): Addition of a and b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+
+    add = int(a) + int(b)
+    return add
