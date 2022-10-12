9b4d11886188f31174cb42cc88412b76fdf3e080
techmate-dot <snwankwo218@gmail.com>
2022-10-13 00:26:39 +0100

    tast 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 const num = process.argv;
 
-function factorial(n) {
-    if (n === 1) {
-      return 1
-    }
-  return factorial(n - 1) * n
+function factorial (n) {
+  if (n === 1) {
+    return 1;
+  }
+  return factorial(n - 1) * n;
 }
 
 const sol = factorial(num[2]);
-console.log(sol)
+console.log(sol);
