4ea53a2a690fe61cc642efe8f40bc2f2fe228f6e
techmate-dot <snwankwo218@gmail.com>
2022-10-14 13:36:28 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const array = args.slice(2).sort();
-const ans = array.slice(-2, -1)[0];
-if (isNaN(ans)) {
-  console.log(0);
-} else {
-  console.log(ans);
-}
+const array = args.slice(2);
+const max = Number(Math.max(...array));
+let smax = 0;
+array.forEach((element) => {
+  if (Number(element) < max && Number(element) > smax) {
+    smax = element;
+  }
+});
+console.log(smax);
