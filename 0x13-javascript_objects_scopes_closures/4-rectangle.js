29e2793ffdf120fc1e9dc52df7c05fbaef162c5d
techmate-dot <snwankwo218@gmail.com>
2022-11-12 12:11:38 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let loop = this.height;
+    while (loop > 0) {
+      console.log('X'.repeat(this.width));
+      loop--;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
