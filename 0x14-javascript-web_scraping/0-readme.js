cd0d964c206c921440f475b9ceb92dbcfb77468a
techmate-dot <snwankwo218@gmail.com>
2022-12-03 09:59:48 +0100

    test

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 
-const path = process.argv[2].toString();
+const path = process.argv[2];
 fs.readFile(path, 'utf-8', function (err, data) {
   if (err) {
     return console.error(err);
   }
-  console.log(data.toString());
+  console.log(data.toString('utf-8'));
 });
