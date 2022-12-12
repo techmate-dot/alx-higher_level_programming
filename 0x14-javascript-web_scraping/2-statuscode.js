a5ffd8bfbc22d3e6d21e39c5646d4ddc05d9c975
techmate-dot <snwankwo218@gmail.com>
2022-12-12 08:58:59 +0100

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const link = process.argv[2].toString()
+const link = process.argv[2].toString();
 
 request(link, function (error, response, body) {
-if (error) {
-  console.error('error:', error);
-}
-console.log('code:',response.statusCode);
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log('code:', response.statusCode);
 }
 );
